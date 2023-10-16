f98fd448a1f963805b4f1de6dda0f1d6d84b15b2
hussain500x <82269242+hussain500x@users.noreply.github.com>
2023-10-16 15:07:23 +0300

    fitst project

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
+*/
+
+
+string[] IDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string name in IDs)
+{
+  if (name.StartsWith("B"))
+  {
+    Console.WriteLine(name);
+
+  }
+}
