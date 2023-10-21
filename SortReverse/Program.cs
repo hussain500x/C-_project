fde416aa87c86898f666b19b58dc1c78a1dcbba0
Hussain Almarzooq <hhaalmarzouq@gmail.com>
2023-10-21 14:00:54 +0300

    some simple project

---
+++ SortReverse/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var a in pallets)
+{
+    Console.WriteLine($"-- {a}");
+}
+
+Console.WriteLine("Reverse...");
+Array.Reverse(pallets);
+foreach (var s in pallets)
+{
+    Console.WriteLine($"{s}");
+}
+
+
---
+++ formatting/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Composite Formatting
+string first = "Hello";
+string second = "World";
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
+
+// string interpolation
+string first2 = "Hello";
+string second2 = "World";
+Console.WriteLine($"{first2} {second2}!");
+Console.WriteLine($"{second2} {first}!");
+Console.WriteLine($"{first2} {first2} {first2}!");
+
+// Formatting currency
+decimal price = 123.45m;
+decimal discount = 50.0m;
+// :C = $
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
